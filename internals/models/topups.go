63fef6de77b0b9674eacb116c03be79b9a9e07cc
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-26 06:30:00 +0800

    feat: create struct create top

---
+++ internals/models/topups.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Topup struct {
+	Id         int64
+	User       int64
+	Amount     float64
+	Status     string
+	References string
+	CreatedAt  time.Time
+	ExpiredAt  time.Time
+	UpdatedAt  time.Time
+}
