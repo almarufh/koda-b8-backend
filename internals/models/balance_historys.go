448f246d1aca10d701550ec4e5573402bfba3cab
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-26 06:05:00 +0800

    feat: create struct balnnce history

---
+++ internals/models/balance_historys.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type BalanceHistory struct {
+	Id         int64
+	User       int64
+	Type       string
+	Amount     float64
+	Before     float64
+	After      float64
+	References float64
+	CreatedAt  time.Time
+}
